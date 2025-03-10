c577daaf1a871fb35dd20158c5d1b888ba7742fb
Simra2004 <simrafaisal103@gmail.com>
2025-03-11 01:29:37 +0500

    message

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,23 @@ import MainInput from "./components/input/input";
 import DoItems from "./components/doitems/doitem";
 
 const App = () => {
+  let [text, setText] = useState("");
+  const handlesubmit = () => {
+   SetListItems([text]);
+  };
+  let[ListItems,SetListItems]=useState([]);
+  console.log(ListItems);
   return (
     <div className="container">
       <div className="header">
         <h1>my to do list</h1>
       </div>
-      <MainInput/> 
-      <DoItems/>
-      
+      <MainInput value={text} setText={setText} />
+      <h4>you typed {text}</h4>
+      <button type="button" className="submit" onClick={handlesubmit}>
+        submit
+      </button>
+      <DoItems />
     </div>
   );
 };
--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,8 +1,15 @@
 import "./input.css";
-const Input = () => {
+const Input = (props) => {
   return (
     <div className="main-input">
-      <input type="text" />
+      <input
+        type="text"
+        value={props.value}
+        onChange={(e) => {
+          console.log(e.target.value);
+          props.setText(e.target.value) //type smthin in input field give event in return , extract  text frm target 
+        }}
+      />
     </div>
   );
 };
